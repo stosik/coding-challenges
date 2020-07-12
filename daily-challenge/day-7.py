139e825d24202cd99ce9ef3fac3bd16bb5f6a6d2
Szymon Tosik <tosikszymon@gmail.com>
2020-07-12 12:45:18 +0200

    Day 7 problem solved

--- daily-challenge/day-7.py
+++ daily-challenge/day-7.py
@@ -1,11 +1,38 @@
 # This problem was asked by Facebook.
-
 # Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
-
 # For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
-
 # You can assume that the messages are decodable. For example, '001' is not allowed.
 
-def solution():
+def memoize(func):
+    cache = dict()
+
+    def memoized_func(*args):
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+
+    return memoized_func
+
+@memoize
+def helper(data, k):
+  if k == 0:
+    return 1
   
-print(solution())
+  s = len(data) - k
+
+  if data[s] == '0':
+    return 0
+  
+  result = helper(data, k -1)
+
+  if k >= 2 and int(data[s:s+2]) <= 26:
+    result += helper(data, k - 2)
+  
+  return result
+
+def solution(data):
+  return helper(data, len(data))
+
+print(solution('111')) # 3
