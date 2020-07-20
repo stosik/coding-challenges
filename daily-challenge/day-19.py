70097e4afe336e24384a8d9cc068f27401bf9ab1
Szymon Tosik <tosikszymon@gmail.com>
2020-07-20 22:11:41 +0200

    Day 19 and 20 completed

--- daily-challenge/day-19.py
+++ daily-challenge/day-19.py
@@ -5,3 +5,44 @@
 
 # Given an N by K matrix where the nth row and kth column represents the cost to build the nth house with kth color,
 # return the minimum cost which achieves this goal.
+
+# N x K (matrix)
+
+
+def solution(costs):
+  if costs == None or len(costs) == 0:
+    return 0
+    
+  n = len(costs)
+  k = len(costs[0])
+
+  min1 = 0
+  min2 = 0
+  idx1 = -1
+
+  for i in range(n):
+    m1 = sys.maxint
+    m2 = sys.maxint
+    idx2 = -1
+
+    for j in range(k):
+      cost = costs[i][j]
+      temp_cost = cost + j
+
+      if temp_cost == idx1:
+        cost = min2
+      else:
+       cost = min1
+
+      if cost < m1:
+        m2 = m1
+        m1 = cost
+        idx2 = j
+      elif cost < m2:
+        m2 = cost
+        
+    min1 = m1
+    min2 = m2
+    idx1 = idx2
+
+    return min1
--- daily-challenge/day-20.py
+++ daily-challenge/day-20.py
@@ -1,9 +1,74 @@
 # This problem was asked by Google.
 
 # Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
-
 # For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, return the node with value 8.
-
 # In this example, assume nodes with the same value are the exact same node objects.
-
 # Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.
+
+
+# 1. Find difference between lenghts of lists
+# 2. Move pointer of longer by difference of lenghts
+# 3. Descendant moving two pointers on lists and comparing their values
+
+class Node:
+  def __init__(self, value=0, next=None):
+    self.value = value
+    self.next = next
+
+
+def list_length(node):
+  current = node
+  count = 0
+
+  while current != None:
+    count += 1
+    current = current.next
+
+  return count
+
+
+def get_intersection_node(d, A, B):
+  i = 0
+  current_one = A
+  current_two = B
+
+  for i in range(d):
+    if current_one == None:
+      return -1
+    current_one = current_one.next
+
+  while current_one != None and current_two != None:
+    if current_one.value == current_two.value:
+      return current_one.value
+    current_one = current_one.next
+    current_two = current_two.next
+
+  return -1
+
+def get_node(A, B):
+  c1 = list_length(A); 
+  c2 = list_length(B); 
+  d = 0
+  
+  if c1 > c2: 
+    d = c1 - c2; 
+    return get_intersection_node(d, A, B)
+  else: 
+    d = c2 - c1; 
+    return get_intersection_node(d, B, A)
+
+
+node_1_a = Node(3)
+node_2_a = Node(7)
+node_3_a = Node(8)
+node_4_a = Node(10)
+node_1_a.next = node_2_a
+node_2_a.next = node_3_a
+node_3_a.next = node_4_a
+
+node_1_b = Node(99)
+node_2_b = Node(1)
+node_1_b.next = node_2_b
+node_2_b.next = node_3_a
+
+print(get_node(node_1_a, node_1_b)) # 8
