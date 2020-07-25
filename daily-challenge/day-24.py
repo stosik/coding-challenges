f4d75b9fb5e8a7615fd655aff15ba9689336ee82
Szymon Tosik <tosikszymon@gmail.com>
2020-07-25 15:12:59 +0200

    Day 24 analyzed and apprehended

--- daily-challenge/day-24.py
+++ daily-challenge/day-24.py
@@ -11,4 +11,53 @@
 # unlock, which unlocks the node. If it cannot be unlocked, then it should return false.
 # Otherwise, it should unlock it and return true.
 # You may augment the node to add parent pointers or any other property you would like.
-# You may assume the class is used in a single-threaded program, so there is no need for actual locks or mutexes. Each method should run in O(h), where h is the height of the tree.
+# You may assume the class is used in a single-threaded program, so there is no need for actual locks or mutexes.
+# Each method should run in O(h), where h is the height of the tree.
+
+class BinaryTreeNode(object):
+  def __init__(self, val, left=None, right=None, parent=None):
+    self.val = val
+    self.left = left
+    self.right = right
+    self.parent = parent
+    self._is_locked = False
+    self.locked_descendants_count = 0
+
+  def _can_lock_or_unlock(self):
+    if self.locked_descendants_count > 0:
+        return False
+
+    cur = self.parent
+    while cur:
+        if cur._is_locked:
+            return False
+        cur = cur.parent
+    return True
+
+  def is_locked(self):
+    return self._is_locked
+
+  def lock(self):
+    if self._can_lock_or_unlock():
+      self._is_locked = True
+      
+      cur = self.parent
+      while cur:
+          cur.locked_descendants_count += 1
+          cur = cur.parent
+      return True
+    else:
+      return False
+
+    def unlock(self):
+      if self._can_lock_or_unlock():
+        self._is_locked = False
+
+        cur = self.parent
+        while cur:
+            cur.locked_descendants_count -= 1
+            cur = cur.parent
+        return True
+      else:
+        return False
+    
