4edadb7c3e889dc613d89d46868408f5bf99cf41
Szymon Tosik <tosikszymon@gmail.com>
2020-08-02 14:51:47 +0200

    Day 29 completed

--- daily-challenge/day-29.py
+++ daily-challenge/day-29.py
@@ -8,6 +8,47 @@
 # You can assume the string to be encoded have no digits and consists solely of alphabetic characters.
 # You can assume the string to be decoded is valid.
 
-def solution():
+class RunLenghtCoder:
+  @staticmethod
+  def encode(message):
+    if message == None or not message or len(message) == 1:
+      return message
 
-print(solution())
+    count = 1
+    encoded_message = ''
+
+    for i in range(1, len(message)):
+      if(message[i] == message[i - 1]):
+        count += 1
+      else:
+        if count > 1:
+          encoded_message += str(count)
+        encoded_message += message[i - 1]
+        count = 1
+    
+    if count > 1:
+      encoded_message += str(count)
+    encoded_message += message[i - 1]
+
+    return encoded_message
+
+  @staticmethod
+  def decode(message):
+    decode = ''
+    count = ''
+    for char in message:
+      if char.isdigit():
+        count += char
+      else:
+        decode += char * int(count)
+        count = ''
+    return decode
+
+
+def solution(message):
+  encoded_message = RunLenghtCoder.encode(message)
+  print(encoded_message)
+  decoded_message = RunLenghtCoder.decode(encoded_message)
+  print(decoded_message)
+
+print(solution('AAAABBBCCDAA'))
