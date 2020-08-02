a751af40a46a2d54385ec7c35737c5e1568abd5f
Szymon Tosik <tosikszymon@gmail.com>
2020-08-02 17:51:21 +0200

    Day 31 done

--- daily-challenge/day-30.py
+++ daily-challenge/day-30.py
@@ -10,6 +10,30 @@
 # Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in the second,
 # and 3 in the fourth index (we cannot hold 5 since it would run off to the left), so we can trap 8 units of water.
 
-def solution():
+def solution(arr): 
+  n = len(arr)
+  result = 0
+       
+  left_max = 0
+  right_max = 0
 
-print(solution())
+  lo = 0
+  hi = n-1
+       
+  while(lo <= hi):  
+    if(arr[lo] < arr[hi]): 
+      if(arr[lo] > left_max): 
+        left_max = arr[lo] 
+      else: 
+        result += left_max - arr[lo] 
+      lo+= 1
+    else: 
+      if(arr[hi] > right_max): 
+        right_max = arr[hi] 
+      else: 
+        result += right_max - arr[hi] 
+      hi-= 1
+          
+  return result 
+
+print(solution([3, 0, 1, 3, 0, 5]))
--- daily-challenge/day-31.py
+++ daily-challenge/day-31.py
@@ -7,6 +7,31 @@
 
 # Given two strings, compute the edit distance between them.
 
-def solution():
+def solution(s1, s2):
+  if len(s1) == 0 or len(s2) == 0:
+    return max(len(s1), len(s2))
 
-print(solution())
+  x = len(s1) + 1
+  y = len(s2) + 1
+
+  A = [[-1 for i in range(x)] for j in range(y)]
+
+  for i in range(x):
+    A[0][i] = i
+
+  for j in range(y):
+    A[j][0] = j
+    for i in range(1, y):
+      for j in range(1, x):
+        if s1[j- 1] == s2[i - 1]:
+          A[i][j] = A[i - 1][j - 1]
+        else:
+          A[i][j] = min(
+            A[i - 1][j],
+            A[i][j - 1],
+            A[i - 1][j - 1]
+          ) + 1
+
+  return A[y - 1][x - 1]
+
+print(solution('kitten', 'sitting'))
